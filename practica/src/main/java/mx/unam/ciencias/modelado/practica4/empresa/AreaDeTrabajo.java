f80299ab41f3037edb2385ba0a0f59017592ed36
Tocny <marcoarubior@ciencias.unam.mx>
2024-09-27 13:20:55 -0600

    Cambios especificos

--- practica/src/main/java/mx/unam/ciencias/modelado/practica4/empresa/AreaDeTrabajo.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica4/empresa/AreaDeTrabajo.java
@@ -2,10 +2,4 @@ package mx.unam.ciencias.modelado.practica4.empresa;
 
 /**Enumeración para las areas de trabajo de los empleados. */
 public enum AreaDeTrabajo{
-    ATENCION_A_CLIENTES,
-    RECURSOS_HUMANOS,
-    CONTABILIDAD,
-    DESARROLLO,
-    MERCADOTECNIA,
-    DIRECCION;
 }
